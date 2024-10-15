d1d2538930b8b71645b6fc8f1fff1096ca084f01
Xenus3 <k.aberbach@hotmail.com>
2024-10-15 12:34:40 +0200

    Express

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const http = require('http')
 const fs = require('fs')
+const path = require('path')
+
+
 const homePage = fs.readFileSync('index.html')
 const aboutPage = fs.readFileSync('about.html')
 const contactPage = fs.readFileSync('contact.html')
 const notFoundPage = fs.readFileSync('notfound.html')
 
 
-const server = http.createServer((req, res) =>{
+const express = require('express') // require express module
+const app = express() // calls express function to start new Express app
+app.use(express.static('public'))
+
+app.listen(3000,()=>{
+console.log("App listening on port 3000")
+})
+
+app.get('/',(req,res)=>{
+   /* res.json({
+    name: 'Greg Lim'
+    })*/ // json response
+
+    res.sendFile(path.resolve(__dirname,'index.html'))
+    })
+
+app.get('/about',(req,res)=>{ // called when request to /about comes in
+        res.sendFile(path.resolve(__dirname,'about.html'))
+        })
+
+app.get('/contact',(req,res)=>{ //called when request to /contact comes
+        res.sendFile(path.resolve(__dirname,'contact.html'))
+        })
+           
+
+
+
+/*const server = http.createServer((req, res) =>{
 if(req.url === '/about')
 res.end(aboutPage)
 else if(req.url === '/contact')
@@ -18,4 +48,4 @@ res.writeHead(404)
 res.end(notFoundPage)
 }
 })
-server.listen(3000)
+server.listen(3000)*/
